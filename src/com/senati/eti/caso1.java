c58d590eafeb7b22625b052766baa3178c1aff08
Alvaro Pezantes <cod.black.opps0@gmail.com>
2021-03-12 16:12:18 -0500

    caso 1 actualizado

--- src/com/senati/eti/caso1.java
+++ src/com/senati/eti/caso1.java
@@ -3,7 +3,7 @@ package com.senati.eti;
 public class caso1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola programador senatino");
+		System.out.println("Holaa programador senatino");
 
 	}
 
